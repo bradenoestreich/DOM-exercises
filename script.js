5d9bc79ff14e4ad80443132afbcff34dffb24d39
Braden <braden.i.oestreich@gmail.com>
2022-09-23 15:34:21 -0400

    you're welcome

---
+++ script.js
@@ -0,0 +1,28 @@
+function init () {
+    const missionAbort = document.getElementById("abortMission");
+    const button = document.getElementById("liftoffButton");
+    const paragraph = document.getElementById("statusReport");
+    
+    // Put your code for the exercises here.
+    button.addEventListener('click', function() {
+        paragraph.innerHTML = 'Houston, we have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', function(event) {
+        event.target.style.backgroundColor = '';
+    });
+
+    missionAbort.addEventListener('click', function() {
+        let result = window.confirm('Are you sure you want to abort the mission?');
+        if (result) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.';
+        }
+    })
+    
+}
+  
+window.addEventListener("load", init);
